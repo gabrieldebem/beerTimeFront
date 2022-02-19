e7bec331a45c43be00f09b93b65cb7750eee62b2
gabrieldebem <gabrieldebem72@gmail.com>
2022-02-19 12:22:09 -0300

    its beer time?

---
+++ src/components/ItsTimeToBeer.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <button 
+            class="bg-gray-500 rounded p-2 text-yellow-500"
+            @click="this.getTimeToBeer()">
+            Its time to Beer?
+        </button>
+    </div>
+    <div class="text-amber-500 text-center mt-4" 
+        v-if="timeToBeer">
+        {{timeToBeer.data.message}}
+    </div>
+    <div v-if="errorMessage">
+        {{errorMessage}}
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    methods: {
+        getTimeToBeer() {
+            axios.get('http://localhost:4000/')
+            .then((response) => {
+                this.timeToBeer = response;
+            })
+            .catch((error) => {
+                console.log(error)
+                this.errorMessage = 'Fail to get correctly data.'
+            })
+        }
+    },
+    data() {
+        return {
+            timeToBeer: '',
+            errorMessage: ''
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
